68580249a3669a21ca82ea66d1fc640556e4c01e
Artem Inkin <arteminkin@yandex.ru>
2025-04-01 03:07:18 +0400

    Разобрался с добавлением иконок в центральное меню

--- src/shared/ui/navbar/Navbar.tsx
+++ src/shared/ui/navbar/Navbar.tsx
@@ -10,7 +10,6 @@ import {DropdownTrigger, DropdownMenu, DropdownItem} from "@heroui/react"
 import dynamic from 'next/dynamic';
 const Dropdown = dynamic(() => import('@heroui/dropdown').then((c) => c.Dropdown), {ssr: false});
 
-
 export const AcmeLogo = () => {
   return (
     <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
@@ -24,11 +23,6 @@ export const AcmeLogo = () => {
   );
 };
 
-
-
-
-
-
 export const SearchIcon = ({size = 24, strokeWidth = 1.5, width="24", height="24", ...props}) => {
     return (
       <svg
@@ -39,8 +33,8 @@ export const SearchIcon = ({size = 24, strokeWidth = 1.5, width="24", height="24
         role="presentation"
         viewBox="0 0 24 24"
         width={width || size}
-        {...props}
-      >
+        {...props} >
+          
         <path
           d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
           stroke="currentColor"
@@ -72,14 +66,14 @@ export const MenuIcon = ({
       xmlns="http://www.w3.org/2000/svg"
       width={size || width}
       height={size || height}
-      viewBox="0 0 24 24"
-      fill="none"
       stroke={stroke}
       strokeWidth={strokeWidth}
+      viewBox="0 0 24 24"
+      fill="none"
       strokeLinecap="round"
       strokeLinejoin="round"
-      {...props}
-    >
+      {...props} >
+
       <path d="M4 6H20" />
       <path d="M4 12H20" />
       <path d="M4 18H20" />
@@ -87,7 +81,23 @@ export const MenuIcon = ({
   );
 };
 
+const HeartIcon = ({size=30, width=30, height=30, stroke="currentColor", ...props}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size || width}
+      height={size || height}
+      stroke={stroke}
+      viewBox="0 0 15 15"
+      fill="none"
+      {...props} >
 
+      <path
+        d="M7.5 13.5L7.14645 13.8536C7.34171 14.0488 7.65829 14.0488 7.85355 13.8536L7.5 13.5ZM1.53553 7.53553L1.18198 7.88909H1.18198L1.53553 7.53553ZM6.53553 2.53553L6.18198 2.88909L6.53553 2.53553ZM7.5 3.5L7.14645 3.85355C7.34171 4.04882 7.65829 4.04882 7.85355 3.85355L7.5 3.5ZM8.46447 2.53553L8.11091 2.18198V2.18198L8.46447 2.53553ZM7.85355 13.1464L1.88909 7.18198L1.18198 7.88909L7.14645 13.8536L7.85355 13.1464ZM13.1109 7.18198L7.14645 13.1464L7.85355 13.8536L13.818 7.88909L13.1109 7.18198ZM6.18198 2.88909L7.14645 3.85355L7.85355 3.14645L6.88909 2.18198L6.18198 2.88909ZM7.85355 3.85355L8.81802 2.88909L8.11091 2.18198L7.14645 3.14645L7.85355 3.85355ZM10.9645 1C9.89418 1 8.86772 1.42517 8.11091 2.18198L8.81802 2.88909C9.38729 2.31981 10.1594 2 10.9645 2V1ZM14 5.03553C14 5.84061 13.6802 6.61271 13.1109 7.18198L13.818 7.88909C14.5748 7.13228 15 6.10582 15 5.03553H14ZM15 5.03553C15 2.80677 13.1932 1 10.9645 1V2C12.6409 2 14 3.35905 14 5.03553H15ZM4.03553 2C4.84061 2 5.61271 2.31981 6.18198 2.88909L6.88909 2.18198C6.13228 1.42517 5.10582 1 4.03553 1V2ZM1 5.03553C1 3.35905 2.35905 2 4.03553 2V1C1.80677 1 0 2.80677 0 5.03553H1ZM1.88909 7.18198C1.31981 6.61271 1 5.84061 1 5.03553H0C0 6.10582 0.42517 7.13228 1.18198 7.88909L1.88909 7.18198Z"
+      />
+    </svg>
+  );
+}
 
 const SVGComponent = () => (
   <svg
@@ -112,11 +122,6 @@ const SVGComponent = () => (
     </g>
   </svg>
 );
-
-
-
-
-
 
 export const NavbarShowcase = () => {  
     const menuItems = [
@@ -161,9 +166,14 @@ export const NavbarShowcase = () => {
 
         {/* Центральная часть: страницы */}  
         <NavbarContent justify="center">
+
           <NavbarItem>
-            <Link color="foreground" href="#">Избранное</Link>
+            <Button disableRipple variant="light" className="flex flex-col items-center justify-center gap-y-0.5 h-12 py-1 hover:text-red-500 hover:!bg-transparent">
+              <HeartIcon className="hover:text-red-500" stroke="currentColor" />
+              <span className="text-xs">Избранное</span>
+            </Button>
           </NavbarItem>
+
           <NavbarItem>
             <Link color="foreground" href="#">Сообщения</Link>
           </NavbarItem>
