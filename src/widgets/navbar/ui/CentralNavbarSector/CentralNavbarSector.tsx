9b32d33c7d8526d6a95a6006416313b78aa8ae87
Artem Inkin <arteminkin@yandex.ru>
2025-04-07 19:46:54 +0400

    Разбил навигационное меню на компоненты

---
+++ src/widgets/navbar/ui/CentralNavbarSector/CentralNavbarSector.tsx
@@ -0,0 +1,32 @@
+import { NavbarContent, NavbarItem } from "@heroui/react";
+import { Button } from "@heroui/button";
+import { HeartIcon } from "@/shared/ui/icons";
+import Link from "next/link";
+
+export const CenrtalNavbarSector = () => {
+  return (
+    <NavbarContent justify="center">
+      {/* Центральная часть: страницы */}
+
+          <NavbarItem>
+            <Button disableRipple variant="light" className="flex flex-col items-center justify-center gap-y-0.5 h-12 py-1 hover:text-red-500 hover:!bg-transparent">
+              <HeartIcon size={25} />
+              <span className="text-xs">Избранное</span>
+            </Button>
+          </NavbarItem> 
+
+          <NavbarItem>
+            <Link color="foreground" href="#">Сообщения</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">Я продаю</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">На карте</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">Для бизнеса</Link>
+          </NavbarItem>
+        </NavbarContent>
+  );
+};
---
+++ src/widgets/navbar/ui/LeftNavbarSector/LeftNavbarSector.tsx
@@ -0,0 +1,30 @@
+import { NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
+import { Input } from "@heroui/input";
+import { LogoIcon, SearchIcon } from "@/shared/ui/icons";
+
+export const LeftNavbarSector = () => {
+  return (
+    <NavbarContent justify="start">
+      {/* Левая часть: лого, название, поиск */}
+      <NavbarItem className="flex gap-5">
+        <NavbarBrand>
+          <LogoIcon />
+          <p className="ml-2 text-xl font-bold text-inherit">MeatWay</p>
+        </NavbarBrand>
+
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[15rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Поиск по объявлениям"
+          size="sm"
+          startContent={<SearchIcon size={20} stroke="#0ea5e9" strokeWidth={2} />}
+          type="search"
+        />
+      </NavbarItem>
+    </NavbarContent>
+  );
+};
